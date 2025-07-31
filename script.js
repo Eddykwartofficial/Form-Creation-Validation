174b9319fbd0447bb028b7a33962fb5bca1223e1
edward kwarteng owusu <108972325+Eddykwartofficial@users.noreply.github.com>
2025-07-31 12:47:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ document.addEventListener('DOMContentLoaded', function()) {
 
 const form = document.getElementById('registration-form');
 console.log('Form Selected:', form);
+
+const feedbackDiv = document.getElementById('form-feedback');
+console.log('feedback div selected:', feedbackDiv);
+
+if (!form) {
+    console.error('Error: Form not found');
+    return;
+}
+
+if (!feedbackDiv) {
+    console.error('Error: Feedback div not found!');
+    return;
+}
+
+form.addEventListener('submit', function(event)) {
+    console.log('Form submission attempted!');
+
+}
+
+event.preventDefault();
+console.log('Default form submission prevented - running validation...');
+
+const username = document.getElementById('username').ariaValueMax.trim();
+const email = document.getElementById('email').ariaValueMax.trim();
