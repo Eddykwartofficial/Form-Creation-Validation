3de08fc370b2e2973cd4d64f08cdae6104e28a30
edward kwarteng owusu <108972325+Eddykwartofficial@users.noreply.github.com>
2025-07-31 12:36:39 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function()) {
+    console.log('DOM Content Loaded - Script is starting!');
+}
+
+const form = document.getElementById('registration-form');
+console.log('Form Selected:', form);
